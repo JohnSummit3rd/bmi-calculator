44cfb8ea672c270e95e908f3950476a0717b15bc
JohnSummit3rd <moyasu2005@gmail.com>
2025-12-28 23:45:29 +1100

    added media query for desktop view and started some of the basic BMI calculation logic in JS

--- script.js
+++ script.js
@@ -2,10 +2,38 @@ const calcPage = document.querySelector('.calc-bmi-page');
 const resultsPage = document.querySelector('.results-bmi-page');
 const calcBmiBtn = document.querySelector('.calc-bmi-btn');
 const editInfoBtn = document.querySelector('.edit-info-btn');
+const heightInput = document.getElementById('height');
+const weightInput = document.getElementById('weight');
+const ageInput = document.getElementById('age');
+const bmiText = document.querySelector(".bmi-text h3");
+const bmiStatus = document.querySelector(".bmi-text p");
+const semiCircle = document.getElementById('semicircle-img');
 
 calcBmiBtn.addEventListener('click', () => {
   calcPage.classList.add('hidden');
   resultsPage.classList.remove('hidden');
+
+  const height = Number(heightInput.value);
+  const weight = Number(weightInput.value);
+  const age = Number(ageInput.value);
+
+  const bmi = (weight / ((height / 100) ** 2)).toFixed(1);
+
+  bmiText.textContent = `${bmi}`;
+
+  if (bmi < 18.5) {
+    bmiStatus.textContent = "Underweight";
+    semiCircle.src = "assets/semicircleblue.svg";
+  } else if (bmi >= 18.5 && bmi < 24.9) {
+    bmiStatus.textContent = "Normal";
+    semiCircle.src = "assets/semicirclegreen.svg";
+  } else if (bmi >= 24.9 && bmi < 29.9) {
+    bmiStatus.textContent = "Overweight";
+    semiCircle.src = "assets/semicircleorange.svg";
+  } else {
+    bmiStatus.textContent = "Obese";
+    semiCircle.src = "assets/semicirclered.svg";
+  }
 });
 
 editInfoBtn.addEventListener('click', () => {
